fa28913b1697828f32f170f7179218be5bcc06d6
Diego Martinez <diegomtzg96@gmail.com>
2020-11-04 01:46:51 -0500

    full fledged portfolio template on django

--- code/webapps/urls.py
+++ code/webapps/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from portfolio import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.portfolio_action, name='portfolio')
 ]
