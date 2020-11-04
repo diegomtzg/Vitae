57be9e413c9665b7c121e2b13ba565aacb8a6215
Diego Martinez <diegomtzg96@gmail.com>
2020-11-04 01:46:51 -0500

    full fledged portfolio template on django

--- code/portfolio/views.py
+++ code/portfolio/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
 # Create your views here.
+def portfolio_action(request):
+    if request.method == 'GET':
+        return render(request, 'portfolio/portfolio.html', {})
