0c727e79c43723b1b3367a513f96ba450f431b70
Diego Martinez <diegomtzg96@gmail.com>
2020-11-14 18:22:14 -0600

    Register and Login functionality, along with initial Profile templating

--- code/portfolio/models.py
+++ code/portfolio/models.py
@@ -1,3 +1,38 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+MAX_CHARFIELD_LENGTH = 50
+
+class Profile(models.Model):
+    owner = models.OneToOneField(User, on_delete=models.CASCADE)
+    profilePic = models.FileField(blank=True)
+    profilePicContentType = models.TextField(default='image/jpg')
+    hasProfilePic = models.BooleanField(default=False)
+
+
+# Parent class for all profile sections
+class ProfileSection(models.Model):
+    sectionName = models.CharField(max_length=MAX_CHARFIELD_LENGTH)
+
+
+class Intro(ProfileSection):
+    # Intro section uses user data to populate contact info
+    bio = models.TextField()
+    # TODO: Add socials
+
+
+class WorkExperience(ProfileSection):
+    companyName = models.CharField(max_length=MAX_CHARFIELD_LENGTH)
+    title = models.CharField(max_length=MAX_CHARFIELD_LENGTH)
+    startDate = models.DateField()
+    endDate = models.DateField() # TODO: Or current?
+    description = models.TextField()
+
+class Education(ProfileSection):
+    schoolName = models.CharField(max_length=MAX_CHARFIELD_LENGTH)
+    location = models.CharField(max_length=MAX_CHARFIELD_LENGTH)
+    degree = models.CharField(max_length=MAX_CHARFIELD_LENGTH)
+    gpa = models.FloatField()
+    gradDate = models.DateField()
+
+# TODO: Add Projects, Skills, Research and Awards
--- code/portfolio/views.py
+++ code/portfolio/views.py
@@ -1,10 +1,92 @@
-from django.shortcuts import render
+from django.conf import settings
+from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.http import HttpResponse
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
-def portfolio_action(request):
-    if request.method == 'GET':
-        return render(request, 'portfolio/portfolio.html', {})
+from django.contrib.auth.models import User
+from portfolio.models import Profile
+from portfolio.forms import RegisterForm, LoginForm
 
-def register_action(request):
+defaultProfilePicPath = settings.ASSETS_ROOT + 'img/defaultProfilePic.jpg'
+
+
+def loginAction(request):
     if request.method == 'GET':
-        return render(request, 'portfolio/dual_registration.html', {})
+        # If user is already logged in, redirect to their profile.
+        if request.user.is_authenticated:
+            return redirect(reverse('profile', args=[0]))
+
+        return render(request, 'portfolio/login.html', {'form': LoginForm()})
+
+    form = LoginForm(request.POST)
+    if not form.is_valid():
+        return render(request, 'portfolio/login.html', {'form': form})
+
+    user = authenticate(username=form.cleaned_data['username'],
+                        password=form.cleaned_data['password'])
+    print("User logged in:", form.cleaned_data['username'])
+    login(request, user)
+    return redirect(reverse('profile', args=[0]))
+
+
+def registerAction(request):
+    # Send empty register form for user to fill out.
+    if request.method == 'GET':
+        return render(request, 'portfolio/register.html', {'form': RegisterForm()})
+
+    # Received register form. Validate and create new user.
+    registerForm = RegisterForm(request.POST)
+    if not registerForm.is_valid():
+        return render(request, 'portfolio/register.html', {'form': registerForm})
+
+    print("Valid register POST request received: ", request.POST)
+    newUser = User.objects.create_user(
+        username=registerForm.cleaned_data['username'],
+        email=registerForm.cleaned_data['email'],
+        password=registerForm.cleaned_data['password'],
+        first_name=registerForm.cleaned_data['firstName'],
+        last_name=registerForm.cleaned_data['lastName'])
+    newUser.save()
+    print("New user created:", newUser)
+
+    # Also create a new profile associated with this user (initially empty).
+    newProfile = Profile()
+    newProfile.owner = newUser
+    newProfile.save()
+    print("New profile created for new user:", newProfile)
+
+    # After registering users, automatically log them in.
+    authUser = authenticate(username=registerForm.cleaned_data['username'],
+                            password=registerForm.cleaned_data['password'])
+    # authUser should never be None since we're using the correct username/password
+    print("Logging new user in automatically...", newUser.username)
+    login(request, authUser)
+
+    # After logging user in, redirect them to their profile.
+    return redirect(reverse('profile', args=[0])) # TODO: Use user.username
+
+
+@login_required
+def visitProfileAction(request, username):
+    if request.method == 'GET':
+        # TODO: Change this to get database data for profile page based on username
+        return render(request, 'portfolio/portfolio.html')
+
+
+@login_required
+def getPhoto(request, username):
+    profile = get_object_or_404(User, username=username).profile
+    return HttpResponse(profile.profilePic, content_type=profile.profilePicContentType)
+
+
+# Used temporarily for debugging...
+def debugLogout(request):
+    logout(request)
+    return redirect(reverse('login'))
+def debugDelete(request):
+    print("Deleting everything...")
+    User.objects.all().delete()
+    Profile.objects.all().delete()
+    logout(request)
+    return redirect(reverse('register'))
