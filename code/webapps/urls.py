18ac215dec19a469772c320fcbf786f649ef32db
Ike Kilinc <ahmetyavuzkilinc@gmail.com>
2020-11-30 13:43:59 -0500

    Updating settings to take secrets from config.ini file & Oauth precursors

--- code/webapps/urls.py
+++ code/webapps/urls.py
@@ -14,12 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from vitae import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.searchAction, name='search'),
+    path('oauth/', include('social_django.urls', namespace='social')),
     path('register/', views.registerAction, name='register'),
     path('login/', views.loginAction, name='login'),
     path('photos/<str:username>/', views.getPhoto, name='photo'),
